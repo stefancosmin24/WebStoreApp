45bf6ea61631c672d2c60b7260a66c1f2313ef4d
Stefan Cosmin <SR1588@live.mdx.ac.uk>
2023-03-20 00:11:53 +0000

    added dist

---
+++ service-worker.js
@@ -0,0 +1,49 @@
+var cacheName = "webstore-v1";
+var cacheFiles = [
+    
+    "index.html",
+    "icons-192.png",
+    "icons-512.png",
+    "icon.png",
+    "icon2.png",
+    "icon3.png",
+    "icon4.png",
+    "icon5.png",
+    "icon6.png",
+    "icon7.png",
+    "icon8.png",
+    "icon9.png",
+    "icon10.png",
+    "icon11.png",
+    "shopping cart.png"
+];
+self.addEventListener("install", function (e) {
+    console.log("[Service Worker] Install");
+    e.waitUntil(
+        caches.open(cacheName).then(function (cache) {
+            console.log("[Service Worker] Caching files");
+            return cache.addAll(cacheFiles);
+        })
+    );
+});
+self.addEventListener("fetch", function (e) {
+    e.respondWith(
+        caches.match(e.request).then(function (cachedFile) {
+            //download the file if it is not in the cache
+            if (cachedFile) {
+                console.log("[Service Worker] Resource fetched from the cache for: " + e.request.url);
+                return cachedFile;
+            } else {
+                return fetch(e.request).then(function (response) {
+                    return caches.open(cacheName).then(function (cache) {
+                        //add the new file to the cache
+                        cache.put(e.request, response.clone());
+                        console.log("[Service Worker] Resource fetched and saved in the cache for: " +
+                            e.request.url);
+                        return response;
+                    });
+                });
+            }
+        })
+    );
+});
